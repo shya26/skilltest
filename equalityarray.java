b682a9844695f16fbbda8efccfe43e4cc84547f2
shya26 <80158841+shya26@users.noreply.github.com>
2021-04-13 22:33:57 +0530

    Add files via upload

---
+++ equalityarray.java
@@ -0,0 +1,51 @@
+import com.sun.deploy.association.Action;
+
+public class equalityarray {
+//    static void equality_checking_two_arrays(int[] my_array1, int[] my_array2) {
+//        boolean equalOrNot = true;
+//
+//        if (my_array1.length == my_array2.length) {import java.util.Arrays;
+//
+//class ArrayEquality {
+//    public static void main( String args[] ) {
+//        int [] arr1 = new int [] {20, 5, -95, 43, 17};
+//        int [] arr2 = new int [] {20, 5, -95, 43, 17};
+//        System.out.println(Arrays.equals(arr1, arr2));
+//    }
+//}
+//            for (int i = 0; i < my_array1.length; i++) {
+//                if (my_array1[i] != my_array2[i]) {
+//                    equalOrNot = false;
+//                }
+//            }
+//        } else {
+//            equalOrNot = false;
+//        }
+//
+//        if (equalOrNot) {
+//            System.out.println("Two arrays are equal.");
+//        } else {
+//            System.out.println("Two  arrays are not equal.");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] array1 = {2, 5, 7, 9, 11};
+//        int[] array2 = {2, 5, 7, 8, 11};
+//        int[] array3 = {2, 5, 7, 9, 11};
+//
+//        equality_checking_two_arrays(array1, array2);
+//        equality_checking_two_arrays(array1, array3);
+//    }
+
+
+    public static void main(String[]args) {
+        int[] arr1 = new int[]{20, 5, -95, 43, 17};
+        int[] arr2 = new int[]{20, 5, -95, 43, 17};
+        System.out.println(arr1.equals(arr1));
+    }
+
+
+}
+
+
